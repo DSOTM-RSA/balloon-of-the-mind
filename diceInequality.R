d69fc01bb889bf11347d809f923b1aa9ab709f59
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2017-12-19 15:19:57 +0100

    added diagnostic plot

--- diceInequality.R
+++ diceInequality.R
@@ -1,12 +1,17 @@
-
+library(tidyverse)
 
 # Kinetic Energy Model
 
 # define parameters
-players <- 200
-interactions<-20000
+players <- 100
+interactions<-10000
 
+rank_01 <- players/100
+rank_50 <- players - (players/2)+1
+
+# build intial game
 dat <- matrix(c(1:players,rep(1000,players),rep(0,players)),nrow=players)
+
 
 # create simulation function
 energies <- function(x){
@@ -35,34 +40,83 @@ energies <- function(x){
     
   }
   
-  vals <- x[,2:3]
+  vals <- x[,1:3]
 
 }
 
 
+
 single_Energy<-energies(dat)
 
-# make holder for long-simulation run
-energies_outcomes <- matrix(ncol=interactions,nrow=players)
+# create log for long-simulation run
+energies_transactions <- matrix(ncol=interactions,nrow=players)
+energies_ranks <- matrix(ncol=interactions,nrow=players)
+
+# diagnostics
+energies_splits <- matrix(ncol=4,nrow=interactions)
 
 # iterate through main loop
 i=1
-for (i in i:ncol(energies_outcomes)) {
+for (i in i:ncol(energies_transactions)) {
   
   results<-energies(dat) # get matrix of results
   
-  trn <-results[,1] # extract transaction
-  cnt <-results[,2] # extract count of transaction
+  # extract transaction and counter
+  trn <-results[,2] 
+  cnt <-results[,3] 
   
   #cnt<-energies(dat[,2])
+  #energies_transactions[,i] <-res
   
-  #energies_outcomes[,i] <-res
-  energies_outcomes[,i] <-trn # pass transaction ledger to array
+  # pass balances to log
+  energies_transactions[,i] <-trn 
   
-  dat[,2]<-energies_outcomes[,i] # assign transaction to balance sheet 
-  dat[,3]<-cnt # assign cnt to balance sheet
+  # pass balances to balance sheet
+  #dat[,2]<-energies_transactions[,i]  
+  dat[,2]<-trn
+  dat[,3]<-cnt 
+  
+  #
+  
+  sorts <- results[order(results[,2],decreasing=TRUE),]
+  energies_ranks[,i] <-sorts[,1]
+  
+  pos01_unit <- sorts[1,1]
+  pos01_val <- sorts[1,2]
+  
+  pos50_val <-sum(sorts[rank_50:players,2])
+  
+  energies_splits[i,1] <-pos01_unit
+  energies_splits[i,2] <-pos01_val
+  energies_splits[i,3] <-pos50_val
+  energies_splits[i,4] <-round((pos01_val/pos50_val)*100,1)
+  
+  
+  
 }  
 
+# calculating length of streaks
+energies_splits_df <- as.data.frame(energies_splits)
+
+# demo of functions for finding mean "rank-size change"
+energies_splits_df %>% group_by(V2,V1) %>% group_size() %>% 
+  cumsum() %>% .[2:length(.)]
+
+# pass above as vector of indices to compute over
+which(energies_ranks[,9]==energies_splits[10,1])
+
+# gives "turnover rate" as a function of interactions 
+energies_splits_df %>% group_by(V2,V1) %>% group_size()%>% 
+  median()/players
+
+
+# main plot
+plot(energies_splits[,4],type="l")
+
+
+
+
+# plot WIP
 outcomes_ranked <-dat[order(dat[,2],decreasing=TRUE),]
 
 rank_01 <- players/100
@@ -72,12 +126,12 @@ trajs_01 <- outcomes_ranked[1:rank_01,1]
 trajs_99 <- outcomes_ranked[rank_99:players,1]
 
 
-paths_01 <- energies_outcomes[trajs_01,]
-paths_99 <- energies_outcomes[trajs_99,]
+paths_01 <- energies_transactions[trajs_01,]
+paths_99 <- energies_transactions[trajs_99,]
 
 
 # plot the data
-par(mfrow=c(1,2))
+par(mfrow=c(1,1))
 
 matplot(t(paths_01),type="l",lty = 1,lwd=0.5,
         ylim=c(0,outcomes_ranked[1,2]+1000),
