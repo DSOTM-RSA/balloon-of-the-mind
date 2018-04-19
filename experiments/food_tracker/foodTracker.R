7d50c925fc12ad1aff6a5af45500ed619af472c3
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2018-04-19 16:53:14 +0200

    draft of Food Tracker: Part 1

--- experiments/food_tracker/foodTracker.R
+++ experiments/food_tracker/foodTracker.R
@@ -1,38 +1,75 @@
 library(readr)
- foodTracker_Winter_18 <- read_delim("foodTracker - Winter_18.tsv", "\t", 
+library(padr)
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+
+setwd("~/Documents/GitArchive/notebook")
+source("data/ROAM.r")
+ 
+foodTracker_Winter_18 <- read_delim("foodTracker - Winter_18.tsv", "\t", 
                                      escape_double = FALSE, 
                                      col_types = cols(Date = col_date(format = "%d/%m/%Y")), 
                                      trim_ws = TRUE)
 
+basics <- read_delim("basics.tsv", "\t",
+                     escape_double = FALSE, 
+                     col_types = cols(Date = col_date(format = "%d/%m/%Y")), 
+                     trim_ws = TRUE)
 
 str(foodTracker_Winter_18)
-library(padr)
-library(tidyverse)
 
-foodTracker_Winter_18 %>% 
+# overview of data 
+kable(head(basics),"html") %>% kable_styling(font_size=12)
+
+# weekly quantities
+basics %>% 
+  thicken(interval = 'week') %>% 
+  group_by(Date_week) %>% 
+  summarise(items_tot=n(),costs_tot=sum(Price)) %>% 
+  arrange(desc(Date_week)) %>% 
+  ggplot(., aes(Date_week,costs_tot)) + 
+  geom_point() + geom_smooth(method = "lm") +
+  theme_plain(base_size = 9)
+
+# weekly visits
+basics %>% 
+  thicken(interval = 'week') %>% 
+  group_by(Date_week) %>% 
+  summarise(items_tot=n(),costs_tot=sum(Price)) %>% 
+  arrange(desc(Date_week)) %>% 
+  ggplot(., aes(items_tot,costs_tot)) + 
+  geom_point() + geom_smooth(method = "lm") +
+  theme_plain(base_size = 9)
+
+# split per category
+basics %>% 
   thicken(interval = 'week') %>% group_by(Category) %>% 
   summarise(Cost = sum(Price)) %>% 
-  arrange(desc(Cost)) 
+  arrange(desc(Cost)) %>% 
+  ggplot(., aes(Category,Cost)) + geom_col() +
+  theme_plain(base_size = 9)
 
-# read in data
-data_Food <- read_delim("foodTracker - Winter_18_2.tsv", "\t", 
-                                    escape_double = FALSE, 
-                                    col_types = cols(Date = col_date(format = "%d/%m/%Y")), 
-                                    trim_ws = TRUE)
+# smoothed distribution of costs
+basics %>% 
+  ggplot(.,aes(Price, ..count.. , 
+               colour=Category, fill = Category)) + 
+  geom_density(position="fill",alpha=0.75,adjust=4) +
+  theme_plain(base_size = 9) + ylab("Proportion of Occurences")
 
-# weekly quants
-week_tot <- data_Food %>% thicken(interval = 'week') %>% 
-  group_by(Date_week) %>% summarise(items_tot=n(),costs_tot=sum(Price))
+
+
+# idea: proportions of product per week (TS)
 
 # building model
-model_data<-data_Food %>% thicken(interval = 'week') %>% 
+model_data<-basics %>% thicken(interval = 'week') %>% 
   mutate(Category=factor(Category)) %>% 
   group_by(Date_week,Category) %>% 
   mutate(un = n()) %>% 
   summarise(cost_Cat=sum(Price),no=n()) %>% 
   mutate(cost_p=round(cost_Cat/sum(cost_Cat)*100,0)) %>% 
   select(-cost_Cat) %>% 
-  left_join(week_tot) %>% 
+  left_join(weekly) %>% 
   ungroup() %>% 
   mutate(Date_week=factor(Date_week))
 
