8b5a30c688698d39cfa1fe62dd84f901e6c13f03
Dan Gray <eoe.ddg.deu@gmail.com>
2019-12-09 16:35:53 +0100

    powershell commands for azure

---
+++ methods/azure/create-multiple-virtual-machines.ps1
@@ -0,0 +1,23 @@
+﻿param([string]$resourceGroup, [int]$total)
+
+# import az module
+Import-Module -name Az
+
+# connect to azure
+Connect-AzAccount
+
+# get user input
+$adminCredential = Get-Credential -Message "Enter a strong admin username and password"
+
+# create virtual machines
+For ($i=1; $i -le $total; $i++)
+{
+
+$vmName = "DemoVM" + $i
+Write-Host "creating VM: " $vmName
+
+New-AzVm -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS 
+
+
+
+} 
---
+++ methods/azure/create-resource-groups.ps1
@@ -0,0 +1,10 @@
+﻿param([string]$rg, [string]$loc)
+
+# import az module
+Import-Module -name Az
+
+# connect to azure
+Connect-AzAccount
+
+# create specific resource group
+New-AzResourceGroup -Name $rg -Location $loc
