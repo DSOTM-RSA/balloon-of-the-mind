291f47985738a18cba7652249ea8fdc582f920f8
Dan Gray <eoe.ddg.deu@gmail.com>
2019-12-13 16:35:01 +0100

    single run checked

--- analysis/drone_survey.R
+++ analysis/drone_survey.R
@@ -1,14 +1,17 @@
 # functions
 
 # calculate area-of-view of a given lens 
-aov_lens <- function(sensor_size, aspect_ratio, focal_length) {
+aov_lens <- function(sensor_size, focal_length) {
+  
+  # set appropriate aspect ratio
+  if (sensor_size == 17.3) {
+    aspect_ratio = 0.75 
+  } else (aspect_ratio = 0.66)
   
   aov_width <- (180*sensor_size)/(pi*focal_length)
-  
   aov_length <- aov_width*aspect_ratio
-  
+
   return(list(aov_width, aov_length))
-  
 }
 
 
@@ -41,6 +44,37 @@ survey_extent <- function(speed, flight_time, half_length, area_block){
 }
 
 
-# in operation 
 
+# TESTING 
 library(purrr)
+
+# define some parameters
+lens <- 13.2 # full frame, m4/3, 1# etc.
+zooms <- c(12,17,24,35,50,100,150) # wide-angle, mid, zoom
+
+aov <-aov_lens(13.2,zooms)
+
+segment <-map2(100,aov,segment_size)
+
+block <-map2(segment[1],segment[2],block_size)
+
+survey <-pmap(list(10,20,segment[2],block),survey_extent)
+
+
+plot(zooms,unlist(survey),type = "b",xlab = "",ylab = "")
+
+
+# IMPLEMENTATION
+# purrr: cross() or expand.grid() --> cross all combinations of sensor vs rest
+
+l <- c(17.31,3.2)
+z <- c(12,17,24,35,100)
+h <- c(15,35,50,100,250)
+s <- c(2.5,4,7.5)
+t <- c(5,12,20)
+
+cross_df <-expand.grid(l,z,h,s,t) %>% 
+  set_names(.,c("lens","zoom","height","speed","time"))
+            
+            
+
